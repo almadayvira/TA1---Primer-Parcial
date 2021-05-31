79de687d47518ac2c2b9df20ecd0026413dfb392
yvira Almada <almadayvira@gmail.com>
2021-05-31 19:53:17 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form id="datos" action="./formulario.php" method="post">
+    </form>
+
+</html>
